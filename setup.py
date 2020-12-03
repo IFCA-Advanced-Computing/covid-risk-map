aba0d4d82f4b8565f6e007eae56ae981169495f7
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2020-12-03 13:01:04 +0100

    Initial commit from cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='COVID-19 risk map based on mobility and socio-demographic data.',
+    author='Advanced Computing and e-Science Group (CSIC)',
+    license='',
+)
